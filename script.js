552166b7f093e199be434c334845b5fc3b2afed8
Anuj Kaushik <aiquiral.anuj@gmail.com>
2021-07-02 12:08:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".line div");
+  const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#606bd3",
+    "#60c2d3"
+  ];
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
+});
